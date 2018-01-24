2726f96c16d2ccb0b4d4db03b5e50e495b7563da
Nicolas Gotchac <ngotchac@gmail.com>
2018-01-24 16:17:02 +0100

    Add parity injection in DEV

--- example/src/Content.js
+++ example/src/Content.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import { Segment } from 'semantic-ui-react';
 
 export default class Content extends Component {
+  state = {
+    blockNumber: 'unknown'
+  };
+
+  componentWillMount () {
+    this.fetchBlockNumber();
+    this.intervalId = setInterval(() => this.fetchBlockNumber(), 2000);
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.intervalId);
+  }
+
+  fetchBlockNumber () {
+    window.ethereum.send('eth_blockNumber', [], (err, val) => {
+      if (err) {
+        return console.error(err);
+      }
+
+      this.setState({ blockNumber: parseInt(val, 16) });
+    })
+  }
+
   render () {
+    const { blockNumber } = this.state;
+
     return (
       <Segment>
-        <img src='/assets/images/wireframe/paragraph.png' alt='Text' />
+        <p>Current block: {blockNumber}</p>
       </Segment>
     );
   }
--- src/inject-html-plugin.js
+++ src/inject-html-plugin.js
@@ -4,12 +4,14 @@ const path = require('path');
 const { replacePlugin } = require('./utils');
 
 const INDEX_PATH = path.resolve(__dirname, 'public/index.ejs');
+const IS_DEV = process.env.NODE_ENV === 'development';
 
 module.exports = function injectHTMLPlugin (config) {
   const htmlPlugin = new HtmlWebpackPlugin(
     {
       inject: true,
-      template: INDEX_PATH
+      template: INDEX_PATH,
+      injectParity: IS_DEV
     }
   );
 
