9675fc29306b61e568fa91f04e4c3bb17478cf8d
Nicolas Gotchac <ngotchac@gmail.com>
2018-02-02 16:56:27 +0100

    Update Ethereum Provider Injection script

--- src/inject-ethereum.js
+++ src/inject-ethereum.js
@@ -15,6 +15,7 @@
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
 import PostMessageProvider from '@parity/api/lib/provider/postMessage';
+import HttpProvider from '@parity/api/lib/provider/http';
 import qs from 'query-string';
 
 function initProvider () {
@@ -45,7 +46,13 @@ function initProvider () {
     appId = path[2];
   }
 
-  const ethereum = new PostMessageProvider(appId);
+  const isIframe = window.top !== window;
+
+  // Use the Post Message provider if in an IFrame,
+  // Otherwise try the HTTP provider with default port
+  const ethereum = isIframe
+    ? new PostMessageProvider(appId)
+    : new HttpProvider('http://127.0.0.1:8545/');
 
   console.log(`Requesting API communications token for ${appId}`);
 
